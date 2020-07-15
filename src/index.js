ac735c6a0e264c4f81f4de77afcef50499ca7890
Sandeep Babu <sandeep.bsn@gmail.com>
2020-07-15 19:03:20 +0530

    Fixed: Page not found

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './App';
 import {store} from './redux/store'
 import {Provider} from 'react-redux'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store = {store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
